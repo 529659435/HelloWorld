a6579c1d3f2d9dfeae4a01461b29a5674f89f153
529659435 <529659435@qq.com>
2019-05-20 15:56:28 +0800

    version:1.5

--- src/zxc.sh
+++ src/zxc.sh
@@ -33,10 +33,10 @@ rm -f $TOMCAT_APP_PATH/webapps/ROOT.war
 rm -f $TOMCAT_APP_PATH/webapps/order.war
 
 # 复制新的工程
-cp $PROJ_PATH/order/target/order.war $TOMCAT_APP_PATH/webapps/
+cp $PROJ_PATH/test2/csDemo.war $TOMCAT_APP_PATH/webapps/
 
 cd $TOMCAT_APP_PATH/webapps/
-mv order.war ROOT.war
+mv csDemo.war ROOT.war
 
 #启动Tomcat
 cd $TOMCAT_APP_PATH/
