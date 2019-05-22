957662d05e98c34e698127738eccb9e1bf3829c5
529659435 <529659435@qq.com>
2019-05-22 11:52:41 +0800

    version:1.6

--- src/zxc.sh
+++ src/zxc.sh
@@ -21,7 +21,7 @@ killTomcat()
     fi
 }
 
-cd $PROJ_PATH/order
+cd $PROJ_PATH/csDemo
 mvn clean install
 
 # 停止tomcat
@@ -30,7 +30,7 @@ killTomcat
 # 删除原有工程
 rm -rf $TOMCAT_APP_PATH/webapps/ROOT
 rm -f $TOMCAT_APP_PATH/webapps/ROOT.war
-rm -f $TOMCAT_APP_PATH/webapps/order.war
+rm -f $TOMCAT_APP_PATH/webapps/mybatis.war
 
 # 复制新的工程
 cp $PROJ_PATH/test2/csDemo.war $TOMCAT_APP_PATH/webapps/
